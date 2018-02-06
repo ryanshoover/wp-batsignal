849318f51d5c82e6e14014fafff9485f95e10bc7
Ryan Hoover <ryan.hoover@wpengine.com>
2018-02-05 21:19:34 -0600

    apply phpcs standards

--- wp-batsignal.php
+++ wp-batsignal.php
@@ -1,19 +1,20 @@
 <?php
-/*
-Plugin Name: WP Batsignal
-Description: Been hacked? Call WordPress Batman to save the day!
-Author: Ryan Hoover
-Version: 0.1
-Author URI: https://ryan.hoover.ws
-*/
+/**
+ * Plugin Name: WP Batsignal
+ * Description: Been hacked? Call WordPress Batman to save the day!
+ * Author: Ryan Hoover
+ * Version: 0.1
+ * Author URI: https://ryan.hoover.ws
+ *
+ * @package wp-batsignal
+ */
 
 if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
 	return;
 }
 
-define( 'WP_BATSIGNAL_PATH', plugin_dir_path( __FILE__ ) );
+// Include our command class.
+require_once plugin_dir_path( __FILE__ ) . '/php/class-wp-batsignal.php';
 
-/**
- * Some files need to be manually loaded
- */
-require_once dirname( __FILE__ ) . '/php/class-wp-batsignal.php';
+// Register the command with WP-CLI.
+WP_CLI::add_command( 'batsignal', 'WP_BatSignal_Command' );
